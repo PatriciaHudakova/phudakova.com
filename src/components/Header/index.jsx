c3c556148efa23370e5225b2ac4dd5a4c97ad20c
PatriciaHudakova <phudakova7@gmail.com>
2020-06-01 23:19:37 +0100

    responsive text added = header complete

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,14 +6,14 @@ const Header = props => {
     return (
         <div className='header'>
             <div className="row">
-                <div className="text">
-                    <h1>Hey There!</h1><br /><br />
-                    <h4>I'm Patricia, and I'm an Information Security graduate <br /> with a keen interest in software engineering!</h4><br /><br />
+                <div className="left-block">
+                    <h1>Hey There!</h1>
+                    <h4>I'm Patricia, and I'm an Information Security graduate <br /> with a keen interest in software engineering!</h4><br />
                     <a href='mailto:phudakova7@gmail.com' type="button" className="btn btn-outline-success btn-lg">
                         Get In Touch!
                     </a>
                 </div>
-                <div className="image">
+                <div className="center-block">
                     <img src={profile} className="rounded-circle" alt="Cinque Terre" />
                 </div>
             </div>
