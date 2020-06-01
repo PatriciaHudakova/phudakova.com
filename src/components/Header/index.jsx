73db3e84a87c0881c8c13bd0befda5b4660ee970
PatriciaHudakova <phudakova7@gmail.com>
2020-06-01 19:43:56 +0100

    get in touch button functional

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,9 +4,11 @@ import './assets/style.scss'
 const Header = props => {
     return (
         <div className='header'>
-            <h1>Title</h1>
-
-            // big button hire me
+            <h1>Hey There!</h1><br /><br />
+            <h4>I'm Patricia, and I'm a software engineer with a <br />keen interest in security!</h4><br /><br />
+            <a href='mailto:phudakova7@gmail.com' type="button" className="btn btn-outline-success btn-lg">
+                Get In Touch!
+            </a>
         </div>
     )
 }
