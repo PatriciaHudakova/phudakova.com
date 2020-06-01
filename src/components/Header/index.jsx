c6041bace4387196c821a7c1bc4e75f643ed2742
PatriciaHudakova <phudakova7@gmail.com>
2020-06-01 22:42:21 +0100

    header image responsive to different screen dimensions

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import './assets/style.scss'
+import profile from './assets/img/profile.png'
 
 const Header = props => {
     return (
         <div className='header'>
-            <h1>Hey There!</h1><br /><br />
-            <h4>I'm Patricia, and I'm a software engineer with a <br />keen interest in security!</h4><br /><br />
-            <a href='mailto:phudakova7@gmail.com' type="button" className="btn btn-outline-success btn-lg">
-                Get In Touch!
-            </a>
+            <div className="row">
+                <div className="text">
+                    <h1>Hey There!</h1><br /><br />
+                    <h4>I'm Patricia, and I'm an Information Security graduate <br /> with a keen interest in software engineering!</h4><br /><br />
+                    <a href='mailto:phudakova7@gmail.com' type="button" className="btn btn-outline-success btn-lg">
+                        Get In Touch!
+                    </a>
+                </div>
+                <div className="image">
+                    <img src={profile} className="rounded-circle" alt="Cinque Terre" />
+                </div>
+            </div>
         </div>
     )
 }
