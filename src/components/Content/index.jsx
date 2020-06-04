fa3e736dbaaf08bea60c9f6407181b0b4f4a6aff
PatriciaHudakova <phudakova7@gmail.com>
2020-06-04 19:11:25 +0100

    timeline icons complete

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -3,6 +3,8 @@ import {Container} from 'reactstrap'
 import './assets/style.scss'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import {IoIosLaptop, IoIosBriefcase} from "react-icons/io";
+import {FaGraduationCap} from "react-icons/fa";
 
 const Content = props => {
     return (
@@ -18,43 +20,112 @@ const Content = props => {
                 <VerticalTimeline>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work align='left'"
+                        date="in progress"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosLaptop />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Restaurant Log</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Hobby Project</h4>
+                        <p>
+                            A platform for people to record, share and look up restaurant experiences so they
+                            don't have to have the same bad experience twice.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work align='left'"
+                        date="June 2020"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosLaptop />}
+                    >
+                        <h3 className="vertical-timeline-element-title">phudakova.com v2.0</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Hobby Project</h4>
+                        <p>
+                            A responsive, fully functional personal website with an entirely different feel created using REACT JS libary.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work align='left'"
+                        date="May 2020"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosLaptop />}
+                    >
+                        <h3 className="vertical-timeline-element-title">phudakova.com v1.0</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Hobby Project</h4>
+                        <p>
+                            A simply static yet fully functional personal website.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work align='left'"
+                        date="April 2020"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosLaptop />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Telegram Event Bot</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Hobby Project</h4>
+                        <p>
+                            A telegram bot created using Golang that helps you keep track of your scheduled events
+                            with full create, edit, delete and reminder functionality.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work align='left'"
+                        date="2018 - 2020"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<FaGraduationCap />}
+                    >
+                        <h3 className="vertical-timeline-element-title">MSc Information Security</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Royal Holloway, University of London</h4>
+                        <p>
+                            Final year project involved exploring web vulnerabilities and their mitigations using a test
+                            website specifically created for that purpose.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work align='left'"
+                        date="2018 - 2019"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosBriefcase />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Teaching Assistant</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Royal Holloway, University of London</h4>
+                        <p>
+                            Assisting with OOP and Software Design labs throughout the year involving
+                            explaining concepts, helping with set-up and marking.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2017 - present"
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<IoIosBriefcase />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Tutor</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Fire-Tech Camp</h4>
+                        <p>
+                            Delivering in person and remote courses for young people including
+                            Video Game Design, AR/VR and Arduino. During my time there, I’ve
+                            been entrusted with assisting with running of the camp.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
                         date="2015 - 2018"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                       // icon={<WorkIcon />}
+                        iconStyle={{ background: 'rgb(217, 83, 79)', color: '#d9534f' }}
+                        icon={<FaGraduationCap />}
                     >
                         <h3 className="vertical-timeline-element-title">BSc Computer Science and Mathematics</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Royal Holloway</h4>
+                        <h4 className="vertical-timeline-element-subtitle">Royal Holloway, University of London</h4>
                         <p>
-                            ll
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2010 - 2011"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        //icon={<WorkIcon />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Art Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                        <p>
-                            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2008 - 2010"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        //icon={<WorkIcon />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                        <p>
-                            User Experience, Visual Design
+                            Final year project involved creating a graph calculator with the following
+                            algorithms: Dijkstra's, Kruskal's, Hamiltonian and Vertex Colouring
+                            as an executable .jar file.
                         </p>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
                 {'/>'}
             </section>
+            <h2 className='row-cols-1 mt-5'>Beside working...</h2><br />
         </Container>
     )
 }
